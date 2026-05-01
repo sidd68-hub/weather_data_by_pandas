f27fe1f2f0ada9b3b95b325b105572594b80af74
Siddharth Nahar <77606988+sidd68-hub@users.noreply.github.com>
2026-05-02 01:56:32 +0530

    CSV file get temperature

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import csv
+
+
 with open('weather_data.csv') as weather:
-    weather_data = weather.readlines()
-    print(weather_data)
+    data = csv.reader(weather)
+    temperature = []
+    for row in data:
+        if(row[1] != "temp"):
+            temperature.append(int(row[1]))
+            print(temperature)
